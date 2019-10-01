edc8f54b75b42a42feaaef98e4fd5ec0a3ddaa2a
catherine-o <catherine.ohara@outlook.com>
2019-09-30 20:19:36 -0600

    enables post request, new user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::UsersController < ApplicationController
-end
+
+    def create
+      @user = User.create(user_params)
+      if @user.valid?
+        render json: { user: UserSerializer.new(@user) }, status: :created
+      else
+        render json: { error: 'Failed to create user' }, status: :not_acceptable
+      end
+    end
+   
+    private
+    def user_params
+      params.require(:user).permit(:username, :name, :password)
+    end
+    
+  end
