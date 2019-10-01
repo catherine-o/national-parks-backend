ebb09b2a13a91919db47b6b0f71438bb63883702
catherine-o <catherine.ohara@outlook.com>
2019-10-01 10:55:44 -0600

    seeds all park records

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,28 @@ User.destroy_all
 
 NPS_KEY = ENV['nps_api_key']
 
-park_base_url = ("https://developer.nps.gov/api/v1/parks?&api_key=" + NPS_KEY)
+park_base_url1 = ("https://developer.nps.gov/api/v1/parks?limit=250&api_key=" + NPS_KEY)
 
-park_results = RestClient.get(park_base_url)
-park_results_array = JSON.parse(park_results)['data']
-park_results_array.each do |park|
+park_results1 = RestClient.get(park_base_url1)
+park_results_array1 = JSON.parse(park_results1)['data']
+park_results_array1.each do |park|
+    Park.create(
+        name: park['name'],
+        designation: park['designation'],
+        states: park['states'],
+        latLong: park['latLong'],
+        description: park['description'],
+        weatherInfo: park['weatherInfo'],
+        url: park['url']
+    )
+end
+
+
+park_base_url2 = ("https://developer.nps.gov/api/v1/parks?limit=250&start=251&api_key=" + NPS_KEY)
+
+park_results2 = RestClient.get(park_base_url2)
+park_results_array2 = JSON.parse(park_results2)['data']
+park_results_array2.each do |park|
     Park.create(
         name: park['name'],
         designation: park['designation'],
