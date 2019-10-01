64018742082c659a0c2c0842d087728bb84b5be1
catherine-o <catherine.ohara@outlook.com>
2019-09-30 22:11:40 -0600

    renders all parks

---
+++ app/controllers/api/v1/parks_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::ParksController < ApplicationController
+    skip_before_action :authorized
+
+    def index
+        @parks = Park.all 
+        render json: @parks 
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :parks, only: [:index]
       resources :users, only: [:create]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
